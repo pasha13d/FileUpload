39fbb8cb725f86740948c9a97fded80e01d675fc
pasha13d <mahabub13d1995@gmail.com>
2021-09-10 17:31:04 +0600

    file byte condition checking

--- FileUploadAPI/FileUploadAPI/Models/FileUploadViewModel.cs
+++ FileUploadAPI/FileUploadAPI/Models/FileUploadViewModel.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,16 +12,29 @@ namespace FileUploadAPI.Models
     {
         public string Message { get; set; }
         public decimal FileSize { get; set; }
+
+        private byte[] fileByte;
+        private static readonly byte[] PDF = { 37, 80, 68, 70, 45, 49, 46 };
+        private static readonly byte[] DOC = { 208, 207, 17, 224, 161, 177, 26, 225 };
+        private static readonly byte[] TEXT = { 71, 70, 71, 10 };
         public bool UploadUserFile([FromForm] IFormFile file)
         {
             bool result = false;
+            if (file.Length > 0)
+            {
+                using (var ms = new MemoryStream())
+                {
+                    file.CopyTo(ms);
+                    fileByte = ms.ToArray();
+                }
+            }
             try
             {
-                bool isValidFileType = string.Equals(file.ContentType, "application/pdf", StringComparison.OrdinalIgnoreCase) ||
+                bool isValidFileType = (string.Equals(file.ContentType, "application/pdf", StringComparison.OrdinalIgnoreCase) && fileByte.Take(7).SequenceEqual(PDF)) ||
                                     string.Equals(file.ContentType, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", StringComparison.OrdinalIgnoreCase) ||
                                     string.Equals(file.ContentType, "application/vnd.ms-excel", StringComparison.OrdinalIgnoreCase) ||
                                     string.Equals(file.ContentType, "text/plain", StringComparison.OrdinalIgnoreCase) ||
-                                    string.Equals(file.ContentType, "application/msword", StringComparison.OrdinalIgnoreCase) ||
+                                    (string.Equals(file.ContentType, "application/msword", StringComparison.OrdinalIgnoreCase) && fileByte.Take(8).SequenceEqual(DOC)) ||
                                     string.Equals(file.ContentType, "application/vnd.openxmlformats-officedocument.wordprocessingml.document", StringComparison.OrdinalIgnoreCase);
                 if (!isValidFileType)
                 {
